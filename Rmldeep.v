5eb56917cfd6b427794cf47d331e1b138ceb717e
Kira Kutscher <kira@tailorsoft.de>
2019-03-04 10:17:17 +0100

    Initial commit. We have a shallow and a deep embedding of Rml.

---
+++ Rmldeep.v
@@ -0,0 +1,137 @@
+Require Import String.
+From mathcomp Require Import all_ssreflect all_algebra.
+From mathcomp Require Import analysis.altreals.distr.
+From mathcomp Require Import analysis.reals.
+From xhl Require Import pwhile.pwhile.
+From xhl Require Import inhabited notations.
+Require Import FunctionalExtensionality.
+
+Reserved Notation "x >>= f" (at level 40, left associativity).
+Class Monad (M : Type -> Type) :=
+  {
+    unit : forall {A}, A -> M A;
+    bind : forall {A B}, M A -> (A -> M B) -> M B
+    where "x >>= f" := (bind x f);
+    monad_law_unit_l : forall {A B} (a : A) (k : A -> M B), unit a >>= k = k a;
+    monad_law_unit_r : forall {A} (x : M A), x >>= unit = x;
+    monad_law_assoc : forall {A B C} (x : M A) (k : A -> M B) (h : B -> M C),
+        x >>= (fun a => k a >>= h) = x >>= k >>= h
+  }.
+
+(* -------------------------------------------------------------------------------- *)
+
+Definition continuation_monad_type := fun ZO A => (A -> ZO) -> ZO.
+Instance continuation_monad ZO : Monad (continuation_monad_type ZO) :=
+  {
+    unit := fun {A} (x : A) (f : A -> ZO) => f x ;
+    bind := fun {A B} (mu : (A -> ZO) -> ZO) (M : A -> (B -> ZO) -> ZO) (f : B -> ZO) => mu (fun (x : A) => M x f)
+  }. 
+Proof. all: reflexivity. Defined.
+
+Definition M := continuation_monad_type. 
+
+Definition expectation_monad_type (R : realType) := M R.
+Instance expectation_monad (R : realType) : Monad (expectation_monad_type R) := continuation_monad R.
+
+Instance option_monad : Monad option :=
+  {
+    unit := @Some ;
+    bind _ _ x f :=
+      match x with
+      | Some y => f y
+      | None => None
+      end
+  }.
+Proof.
+  all: try destruct x.
+  all: reflexivity.
+Qed.
+
+Check @None.
+
+(* -------------------------------------------------------------------------------- *)
+
+(* Inductive Rml {A} : Type :=
+   | Var : nat -> @Rml A 
+   | Const : A -> @Rml A
+   | Let_stm : forall B, nat -> @Rml B -> @Rml A -> @Rml A
+   (* | Fun_stm : forall B, nat -> B -> @Rml A -> @Rml A *)
+   | If_stm : @Rml bool -> @Rml A -> @Rml A -> @Rml A
+   | App_stm : forall B, @Rml (B -> A) -> @Rml B -> @Rml A. *)
+
+
+Inductive RMLtype : Type :=
+| RMLnat 
+| RMLbool 
+| RMLfun of RMLtype & RMLtype.
+
+Fixpoint interpType (t : RMLtype) : Type :=
+  match t with
+  | RMLnat => nat
+  | RMLbool => bool
+  | RMLfun t1 t2 => interpType t1 -> (M R (interpType t2))
+  end. 
+  
+Inductive RMLconst : Type :=
+| bVal of bool
+| nVal of nat.
+
+Inductive Rml : Type :=
+| Var of nat                       (* variable identifier *)
+| Const of RMLconst               (* value of the constant *) 
+| Let_stm of nat & Rml & Rml       (* let string = Rml in Rml *)
+| If_stm of Rml & Rml & Rml        (* if bool then Rml else Rml *)
+. 
+
+
+(* | Fun_stm : forall B, nat -> B -> @Rml A -> @Rml A 
+   | If_stm : @Rml bool -> @Rml A -> @Rml A -> @Rml A
+   | App_stm : forall B, @Rml (B -> A) -> @Rml B -> @Rml A. *)
+
+(* -------------------------------------------------------------------------------- *)
+  
+(*Compute (fun env => ((fun y => if y == "x"
+                        then 3
+                        else env y) "x")+2).
+
+Let_stm x := 3 in x + 2.
+
+
+Compute (fun x => x).
+Compute (fun x => *)
+
+(*Inductive env : Type := forall A, (forall B, B -> option A).
+Compute (Const (fun x => x 3 2))*)
+
+
+Fixpoint interp (x : Rml) env : M R RMLconst :=
+  match x with
+  | Var y => unit (env y)
+  | Const c => unit c
+  | Let_stm x a b => bind (interp a env) 
+                         (fun x' => interp b 
+                                        (fun y => if x == y
+                                               then x'
+                                               else env y))
+  (* | App_stm B f x => bind (interp f env) (fun (g : B -> A) => bind (interp x) (fun k => unit (g k)))*)
+  | If_stm b m1 m2 => bind (interp b env)
+                          (fun (t : RMLconst) =>
+                             match t with
+                             | bVal true => interp m1 env
+                             | bVal false => interp m2 env
+                             | _ => unit (bVal false)
+                             end) 
+  end.
+
+
+(* Compute interp (App_stm nat (Const (fun x => x + 10)) (Const 2)).
+Compute interp (If_stm (App_stm nat (Const (fun x => x > 10)) (Const 2)) (Const 3) (Const 2)).*)
+Example test1 :
+  interp (Let_stm 0 (Const (nVal 4)) (Var 0)) (fun x => bVal false) =
+  interp (Const (nVal 4)) (fun x => bVal false).
+Proof. reflexivity. Qed. 
+
+(Const (fun e => 2) e)
+
+Definition punit {R} {A} := @unit (expectation_monad_type R) (expectation_monad R) A.
+Definition pbind {R} {A B} := @bind (expectation_monad_type R) (expectation_monad R) A B.
---
+++ dmonad.v
@@ -0,0 +1,72 @@
+(* -------------------------------------------------------------------- *)
+(* ------- *) Require Import ClassicalFacts Setoid Morphisms.
+From mathcomp Require Import all_ssreflect all_algebra.
+From mathcomp Require Import boolp classical_sets reals distr.
+
+Set   Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+Unset SsrOldRewriteGoalsOrder.
+Set   Printing Projections.
+
+Import GRing.Theory Num.Theory.
+
+Local Open Scope ring_scope.
+
+(* -------------------------------------------------------------------- *)
+Parameter (R : realType).
+
+(* -------------------------------------------------------------------- *)
+Lemma funext : forall {T U : Type} (f g : T -> U),
+  (forall x, f x = g x) -> f = g.
+Proof. by move=> T U f g; apply: functional_extensionality_dep. Qed.
+
+Lemma funext2 : forall {T U V : Type} (f g : T -> U -> V),
+  (forall x y, f x y = g x y) -> f = g.
+Proof. by move=> T U V f g eq; apply/funext=> x; apply/funext. Qed.
+
+(* -------------------------------------------------------------------- *)
+Lemma distr_eqP {T : choiceType} (f1 f2 : {distr T / R}) :
+  f1 =1 f2 <-> f1 = f2.
+Proof. 
+split=> [|->] //; case: f1 f2 => [mu1 ge0_1 s1 le1_1] [mu2 ge0_2 s2 le1_2].
+move/funext => /= eq_mu; have PI (A : Prop): A \/ ~ A by case: (asboolP A); auto.
+by subst; congr Distr; apply Prop_irrelevance.
+Qed.
+
+(* -------------------------------------------------------------------- *)
+Definition Choice T := (ChoiceType (EqType T gen_eqMixin) gen_choiceMixin).
+
+(* -------------------------------------------------------------------- *)
+Record premonad := mkPreMonad {
+  M    :> Type -> Type;
+  unit :  forall {T : Type}, T -> M T;
+  bind :  forall {T U : Type}, M T -> (T -> M U) -> M U;
+}.
+
+Arguments unit p {T} _.
+Arguments bind p {T U} _ _.
+
+(* -------------------------------------------------------------------- *)
+Variant ismonad (M : premonad) :=
+| IsMonad of
+       (forall {T U} (x : T) (f : T -> M U),
+          M.(bind) (M.(unit) x) f = f x)
+     & (forall {T} (m : M T),
+          M.(bind) m M.(unit) = m)
+     & (forall {T U V} (m : M T) (f : T -> M U) (g : U -> M V),
+          M.(bind) (M.(bind) m f) g = M.(bind) m (fun x => M.(bind) (f x) g)).
+
+(* -------------------------------------------------------------------- *)
+Definition dpremonad :=
+  {| M    := fun T => {distr (Choice T) / R}
+   ; unit := fun T => @dunit R (Choice T)
+   ; bind := fun T U m f => @dlet R (Choice T) (Choice U) f m |}.
+
+(* -------------------------------------------------------------------- *)
+Lemma ismonad_dpremonad : ismonad dpremonad.
+Proof. constructor => [T U x f|T m|T U V m f g] /=.
++ by apply/distr_eqP/dlet_unit.
++ by apply/distr_eqP/dlet_dunit_id.
++ by apply/distr_eqP/dlet_dlet.
+Qed.
